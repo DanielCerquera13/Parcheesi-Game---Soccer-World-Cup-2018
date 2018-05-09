bad1dc2b5c6f12c2078f6ccee6b42bbd3fe636df
HP <HP@172.30.185.248>
2018-05-09 14:49:00 -0500

    Se crean los atributos, el constructor y los metodos get y set de la clase jugador

--- Proyecto APO II/src/modelo/Jugador.java
+++ Proyecto APO II/src/modelo/Jugador.java
@@ -1,5 +1,64 @@
 package modelo;
 
 public class Jugador {
+	
+	//ATRIBUTOS
+	
+	/**
+	 * Atributo que representa el nombre del usuario
+	 */
+	private String nickName;
+	
+	/**
+	 * Atributo que representa la contrasena del usuario
+	 */
+	private String contrasena;
+	
+	
+	
+	//CONSTRUCTOR
+	
+	public Jugador(String nickName, String contrasena) {
+		
+		this.nickName = nickName;
+		this.contrasena = contrasena;
+	}
+	
+
+	// GET AND SET
+
+	/**
+	 * Este metodo retorna el nombre del jugador
+	 * @return String nickName.
+	 */
+	public String getNickName() {
+		return nickName;
+	}
+	
+	/**
+	 * Este modifica el nombre del jugador
+	 * @param nickName nombre del jugador.
+	 */
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	
+	/**
+	 * Este metodo retorna la contrasena del jugador
+	 * @return String contrasena.
+	 */
+	public String getContrasena() {
+		return contrasena;
+	}
+	
+	/**
+	 * Este modifica la contrasena del jugador
+	 * @param contrasena contrasena del jugador del jugador.
+	 */
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
 
 }
