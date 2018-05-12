f26bf9616fd36f68e89d02459b5ef20b5a7501be
juanchovelezpro <38393696+juanchovelezpro@users.noreply.github.com>
2018-05-12 17:51:51 -0500

    Cambios Ventana Principal - Titulo -Resizable- Default close

--- Proyecto APO II/src/interfaz/VentanaPrincipal.java
+++ Proyecto APO II/src/interfaz/VentanaPrincipal.java
@@ -2,27 +2,26 @@ package interfaz;
 
 import javax.swing.*;
 
-
 import java.awt.*;
 
-public class VentanaPrincipal extends JFrame  {
-  
+public class VentanaPrincipal extends JFrame {
+
 	private VentanaIniciarSesion ventanaSesion;
 	private VentanaNuevoUsuario ventanaUsuario;
-	
-	public VentanaPrincipal () {
-		
-		
-		  
-		setSize(1280, 1024); 
-		
+
+	public VentanaPrincipal() {
+
+		setTitle("Parqués Mundial Rusia 2018");
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setSize(1280, 1024);
+
 		ventanaSesion = new VentanaIniciarSesion();
 		ventanaUsuario = new VentanaNuevoUsuario();
-		
-				
+
 	}
-	
-	
+
 	public static void main(String args[]) {
 		VentanaPrincipal ventana = new VentanaPrincipal();
 		ventana.setVisible(true);
