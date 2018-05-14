c60322e7e7fa075ac4b386f64bd79e07eefd2ff3
juandavidpaz <juandavidpaz@192.168.0.10>
2018-05-13 20:02:42 -0500

    0l2

--- Proyecto APO II/src/interfaz/PanelNuevoUsuario.java
+++ Proyecto APO II/src/interfaz/PanelNuevoUsuario.java
@@ -43,17 +43,12 @@ public class PanelNuevoUsuario extends JPanel implements ActionListener {
 	}
 
 	public void inicializarComponentes() {
-		   nombre = new JLabel("Nombre");
-	        nombre.setBounds(936, 180, 400, 80);
-	        nombre.setFont(new Font("Garamond", 1, 40));
-	        nombre.setForeground(Color.WHITE);
-	        
-	        nombretxt = new JTextField();
-	        nombretxt.setBounds(936, 280, 400, 80);
-	        nombretxt.setFont(new Font("Garamond", 1, 40));
-	        
 		
-		contrasena = new JLabel("Contraseña");
+		
+		
+		
+		  
+		
 
 		nombre = new JLabel("Nombre");
 		nombre.setBounds(936, 180, 400, 80);
@@ -75,12 +70,7 @@ public class PanelNuevoUsuario extends JPanel implements ActionListener {
         contrasenatxt.setFont(new Font("Garamond", 1, 40));
         
         
-        
-        
-		contrasenaConfirmar = new JLabel("Confirmar contraseña");
-		contrasenatxt = new JPasswordField();
-		contrasenatxt.setBounds(936, 480, 400, 80);
-		contrasenatxt.setFont(new Font("Garamond", 1, 40));
+       
 
 		contrasenaConfirmar = new JLabel("Confirmar contrasena");
 		contrasenaConfirmar.setBounds(936, 580, 500, 80);
@@ -112,24 +102,11 @@ public class PanelNuevoUsuario extends JPanel implements ActionListener {
         botonRegresar.setActionCommand(REGRESAR);
 		
 
-		contrasenaConfirmartxt = new JPasswordField();
-		contrasenaConfirmartxt.setBounds(936, 680, 400, 80);
-		contrasenaConfirmartxt.setFont(new Font("Garamond", 1, 40));
-
-		botonComenzar = new JButton(COMENZAR);
-		botonComenzar.setBounds(236, 500, 400, 80);
-		botonComenzar.setFont(new Font("Garamond", 1, 40));
-		botonComenzar.addActionListener(this);
-		botonComenzar.setActionCommand(COMENZAR);
 
 		//fd
 		// 136 330
 
-		botonRegresar = new JButton(REGRESAR);
-		botonRegresar.setBounds(236, 700, 400, 80);
-		botonRegresar.setFont(new Font("Garamond", 1, 40));
-		botonRegresar.addActionListener(this);
-		botonRegresar.setActionCommand(REGRESAR);
+	
 
 	}
 
