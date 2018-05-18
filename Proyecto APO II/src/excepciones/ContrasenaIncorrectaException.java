b3a517815f4ef9d897bb87bf3dec855f1ca052ac
juandavidpaz <juandavidpaz@192.168.0.10>
2018-05-18 17:38:54 -0500

    no metan la puta ñ

---
+++ Proyecto APO II/src/excepciones/ContrasenaIncorrectaException.java
@@ -0,0 +1,13 @@
+package excepciones;
+
+
+public class ContrasenaIncorrectaException extends Exception {
+
+	public ContrasenaIncorrectaException() {
+		
+	super("La contrase�a es incorrecta");	
+		
+	}
+	
+	
+}
