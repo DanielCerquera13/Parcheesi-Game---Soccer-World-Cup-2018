03b4c20e7e68825a489b3d22b18b4246393373c3
juandavidpaz <juandavidpaz@192.168.0.11>
2018-05-25 18:55:06 -0500

    casa

--- Proyecto APO II/src/modelo/CasillaCasa.java
+++ Proyecto APO II/src/modelo/CasillaCasa.java
@@ -7,4 +7,27 @@ public class CasillaCasa  extends Casilla {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	
+	
+	
+public void agregarFicha(Ficha ficha) {
+		
+		int tamanio = casilllasOcupadas();
+		if(hayFichas() == false) {
+			getFichas()[0]=ficha;
+		}
+		
+		else if(hayFichas() == true) {
+			
+			for (int i = 0; i < getFichas().length; i++) {
+			      
+			if(getFichas()[i].getTipo()== ficha.getTipo()) {
+					getFichas()[tamanio+1]=ficha;
+				}
+				
+		}
+	}
+	
+	}
 }
