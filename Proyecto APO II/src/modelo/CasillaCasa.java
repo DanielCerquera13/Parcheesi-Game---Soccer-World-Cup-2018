20784562b0657d49b670ea3af3668b484ff508d9
juanchovelezpro <38393696+juanchovelezpro@users.noreply.github.com>
2018-05-25 19:10:53 -0500

    Cambio CasillaCasa

--- Proyecto APO II/src/modelo/CasillaCasa.java
+++ Proyecto APO II/src/modelo/CasillaCasa.java
@@ -1,34 +1,29 @@
 package modelo;
 
-public class CasillaCasa  extends Casilla {
+public class CasillaCasa extends Casilla {
 
 	private int tipoFicha;
+
 	public CasillaCasa(int tipo, int tipoFicha) {
 		super(tipo);
 		this.tipoFicha = tipoFicha;
 	}
 
-	
-	
-	
-	
-public void agregarFicha(Ficha ficha) {
-		
+	public void agregarFicha(Ficha ficha) {
+
 		int tamanio = casilllasOcupadas();
-		if(hayFichas() == false) {
-			getFichas()[0]=ficha;
+
+		if (tipoFicha == ficha.getTipo()) {
+			if (hayFichas() == false) {
+				getFichas()[0] = ficha;
+			}
+
+			else if (hayFichas() == true) {
+
+				getFichas()[tamanio + 1] = ficha;
+
+			}
 		}
-		
-		else if(hayFichas() == true) {
-			
-			for (int i = 0; i < getFichas().length; i++) {
-			  if(tipoFicha == ficha.getTipo())    
-				
-					getFichas()[tamanio+1]=ficha;
-				}
-				
-		}
-	
-	
+
 	}
 }
